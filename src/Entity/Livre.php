6bf26b2a7213448f55daa7859af4b0ac85f95be8
Harry7986 <emarh.harry.code@gmail.com>
2026-03-05 22:01:02 +0100

    Livre Entity

---
+++ src/Entity/Livre.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LivreRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LivreRepository::class)]
+class Livre
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
